e6f8b050563ee0107f1c3c67f22a98e216cc6f9a
Rumen Petrov <rumenpetrow@gmail.com>
2026-05-25 10:18:45 +0300

    Upgrade Astro and add React support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rumenpetrov.github.io",
   base: "/serverless-notes",
+  integrations: [react()],
 });

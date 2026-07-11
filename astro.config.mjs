64730933f6649fb6b9c7cae6dd8c5f7422c186ce
Rumen Petrov <rumenpetrow@gmail.com>
2026-07-11 13:40:19 +0300

    Clean up react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://rumenpetrov.github.io",
-  base: "/serverless-notes",
-  integrations: [react()],
+  base: "/serverless-notes"
 });
